660e58da5d7f3ef32a24bd2a39f03c39313ddd8c
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-10-15 17:06:41 +0530

    Add files via upload

---
+++ Arithmetic operaters.py
@@ -0,0 +1,12 @@
+print(45+56+78+90+100) #addition
+
+print(70-100-45-67)#subtraction
+
+print(30*80*60*70*55) #multiplication
+
+print(156873/3) #division
+
+print(200//3) #floordivision(the output will be the number before decimal point after dividing)
+
+print(34565%5) #modulus( it shows remainder)
+
---
+++ assignmentoperators.py
@@ -0,0 +1,41 @@
+a=300
+
+a+=500 #it is same as a=a+5
+
+print(a)
+
+a-=200 #it is same as a=a-200
+
+print(a)
+
+a*=50 # it is same as a=a*50
+
+print(a)
+
+a/=60 # it is same as a/60
+
+print(a)
+
+a%=77 #it is same as a%77
+
+print(a)
+
+a//=96 #it is same as a=a//96
+
+print(a)
+
+a**=45 # is same as a**45
+
+print(a)
+
+#expotential
+
+x=50
+y=78
+print(x**y) #this means 50 multiply 78 times
+
+x=2
+y=3
+print(x**y) #this means 2*2*2
+
+
