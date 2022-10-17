8dfec2b51368e4d9808f787426c805c1c333f3bc
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-10-17 21:52:07 +0530

    Add files via upload

---
+++ bitwise operators.py
@@ -0,0 +1,26 @@
+#these are used to compare binary numbers 
+
+#& --->AND-->if both are 1, it sets each bit to 1
+
+#!-->or--> if one of the two bits is 1,it sets each bit to 1
+
+#^-->XOR-->if only one of two bits is 1,its sets each bit to 1
+
+#-->NOT-->inverts all bits -->complement operators--its returns ones complement
+
+# << => Zero fill left shift => the binary number is appended with 0's at the end
+
+# >> => Right SHift => In simple terms, the right side of the bits are removed
+
+
+x = 10
+y = 7
+
+print(x | y)
+
+a = 78
+b = 89
+
+print(a^b)
+
+print(round(0.5)-round(-0.5))
