79e68f203451b852e8443d308132f809508e5c55
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-10-22 23:43:23 +0530

    Add files via upload

---
+++ pythonpractise01.py
@@ -0,0 +1,66 @@
+#practise python
+
+x="I want to practise coding"
+
+text = x.split()
+
+print( text [2])
+
+x="my love"
+c=x.split()
+#here split function uses to separate thwe words if we nit use the spilt function it will count the letters
+
+print(c[1])
+
+
+y="my love is towardsa ypu my darling "
+print(y.replace("p","o"))
+#replace function replaces the letters in the sentence 
+
+a=5678
+a+=567
+print(a)
+
+g=908
+g-=678
+print(g)
+
+f=9087
+f*=345
+print(f)
+
+r=6789
+y=10
+print(r**y) #exponential form
+
+
+v=56
+print(v<12 and v<34)
+print(v<78 or v>78)
+print(v>76 or a)
+
+m=1010
+n=1110
+print(m|n)
+print(m^n)
+print(m-n)
+
+
+y=1209
+print(float(y))
+
+print(complex(y))
+
+print(int(y))
+
+D=123.56
+
+print(int(D))
+print(complex(D))
+
+
+
+
+
+
+
