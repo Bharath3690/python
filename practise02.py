c72f7f211456bda77423be91f20797a3fe2a281d
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-10-23 23:08:33 +0530

    Add files via upload

---
+++ practise02.py
@@ -0,0 +1,16 @@
+#input function of python in these we can write the output in terminal part
+
+#THe output of a input function is always a string(even number is entered also)
+
+#this function takes take input from the keyboard
+
+from re import X
+
+
+a=input("Enter your name: ")
+
+print("my name is: ",a) #in these print function  we write the string we want then write the a
+
+
+Name="bharath"
+print (Name [1:3])
