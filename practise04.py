5c4101d7cca64b4aeb829e73e960ddf1f60e4c90
Bharath3690 <115339747+Bharath3690@users.noreply.github.com>
2022-10-25 23:48:17 +0530

    Add files via upload

---
+++ practise04.py
@@ -0,0 +1,28 @@
+#practise 04
+
+#computeAverage.py
+#x=eval(input("Enter one number: "))
+
+#y=eval(input("Enter Second number: "))
+
+#z=eval(input("Enter Third Number: "))
+
+#average = (x+y+z)/3
+
+#print("The average of",x,y,z,"is",average)
+
+#compute Area.py
+
+radius=20
+
+area=radius*radius*3.14159
+
+print("The area for the circle of radius",radius,"is",area)
+
+#compute area with radius with input
+
+radius=eval(input("Enter the value of radius: "))
+
+area=radius*radius*3.14159
+
+print("area of circle",radius,"is",area)
